525f79ff273a119331c70c3ddaf35d8a07d91902
Jenny Bao <jennybao1004@gmail.com>
2020-11-27 17:26:40 -0500

    sentry

--- hosuto/settings.py
+++ hosuto/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'hosuto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG is True:
+if DEBUG:
 
     DATABASES = {
         'default': {
