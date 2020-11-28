98bb467ddc8f8f3f6a3df0de74d2c5640ae8abfc
Jenny Bao <jennybao1004@gmail.com>
2020-11-28 11:29:12 -0500

    static url

--- hosuto/settings.py
+++ hosuto/settings.py
@@ -195,6 +195,8 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET")
     AWS_STORAGE_BUCKET_NAME = "hosuto-bucket"
+     AWS_BUCKET_ACL = "public-read"
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com/uploads"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
